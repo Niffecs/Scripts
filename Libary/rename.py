9f170d8849e834c35f95ed52194ec652004901da
Niffecs <Niffecs@gmail.com>
2022-12-08 21:53:50 +0100

    Fix wget Bug in WSL

--- Libary/rename.py
+++ Libary/rename.py
@@ -12,42 +12,46 @@ path_dir = path_dir.replace("\\", "//")
 
 # Delete end of File
 for runner in os.listdir(path_dir):
-    # Delete Libary ending
-    os.rename(f"{path_dir}//{runner}",
-              f"{path_dir}//{runner}".replace(libary_name, ""))
+    if not ".py" in runner:
+        # Delete Libary ending
+        os.rename(f"{path_dir}//{runner}",
+                f"{path_dir}//{runner}".replace(libary_name, ""))
 
 # Rename Umlaute
 runner = False
 for runner in os.listdir(path_dir):
-    flag = runner
-    runner = runner.replace("ä", "ae").replace("ö", "oe").replace(
-        "ü", "ue").replace("ß", "ss").replace("•", "")
-    os.rename(f"{path_dir}//{flag}", f"{path_dir}//{runner}")
+    if not ".py" in runner:
+        flag = runner
+        runner = runner.replace("ä", "ae").replace("ö", "oe").replace(
+            "ü", "ue").replace("ß", "ss").replace("•", "")
+        os.rename(f"{path_dir}//{flag}", f"{path_dir}//{runner}")
 
 # Delete Author
 runner = False
 for runner in os.listdir(path_dir):
-    start = runner.find("(")
-    if ".pdf" in runner:
-        new_name = f"{runner[0:start]}.pdf"
-    elif ".epub" in runner:
-        new_name = f"{runner[0:start]}.epub"
-    else:
-        print(f"Error\n\t-->{runner}")
-        sys.exit(0)
-    os.rename(f"{path_dir}//{runner}", f"{path_dir}//{new_name}")
+    if not ".py" in runner:
+        start = runner.find("(")
+        if ".pdf" in runner:
+            new_name = f"{runner[0:start]}.pdf"
+        elif ".epub" in runner:
+            new_name = f"{runner[0:start]}.epub"
+        else:
+            print(f"Error\n\t-->{runner}")
+            sys.exit(0)
+        os.rename(f"{path_dir}//{runner}", f"{path_dir}//{new_name}")
 
 # delete WhiteSpace
 runner = False
 for runner in os.listdir(path_dir):
-    flag = runner
-    if ".pdf" in runner:
-        runner = runner.replace(" .pdf", ".pdf")
-    elif ".epub" in runner:
-        runner = runner.replace(" .epub", ".epub")
-    elif ".py" in runner:
-        runner = ".py" # ignore
-    else:
-        print(f"Error\n\t-->\t{runner}")
-    runner = runner.replace(" ", "_")
-    os.rename(f"{path_dir}//{flag}", f"{path_dir}//{runner}")
+    if not ".py" in runner:
+        flag = runner
+        if ".pdf" in runner:
+            runner = runner.replace(" .pdf", ".pdf")
+        elif ".epub" in runner:
+            runner = runner.replace(" .epub", ".epub")
+        elif ".py" in runner:
+            runner = ".py" # ignore
+        else:
+            print(f"Error\n\t-->\t{runner}")
+        runner = runner.replace(" ", "_")
+        os.rename(f"{path_dir}//{flag}", f"{path_dir}//{runner}")
