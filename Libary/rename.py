2ee4b5fbf93cfe2fcf650309ca1b81cab5f171a3
Niffecs <117777659+Niffecs@users.noreply.github.com>
2022-12-12 02:46:55 +0100

    Update rename.py

--- Libary/rename.py
+++ Libary/rename.py
@@ -1,3 +1,12 @@
+# /usr/bin/env python3
+
+# There is no beauty award or anything like that for the following source code.
+# It fulfills the task I set for the source code.
+# Since this is fulfilled, I am satisfied.
+# As you can see, the code is written blockwise, so you can replace or extend certain blocks.
+# I do not guarantee the correctness of the code written here.
+# As said. It works for me, so be careful when you run it on your end.
+
 import os
 import sys
 import base64
@@ -9,6 +18,20 @@ libary_name = base64.b64decode(libary_name).decode('utf-8')[::-1]
 # Path
 path_dir = "."
 path_dir = path_dir.replace("\\", "//")
+
+
+# Test Folder
+error = 0
+for runner in os.listdir(path_dir):
+    if not "pdf" in runner:
+        if not "epub" in runner:
+            if not "py" in runner:
+                error += 1
+                print("Unbekannte Datei:\n")
+                print(f"\t{runner}\n")
+                print("Stop")
+if error != 0:
+    sys.exit(0)
 
 # Delete end of File
 for runner in os.listdir(path_dir):
@@ -23,18 +46,17 @@ for runner in os.listdir(path_dir):
     if not ".py" in runner:
         flag = runner
         runner = runner.replace("ä", "ae").replace("ö", "oe").replace(
-            "ü", "ue").replace("ß", "ss").replace("•", "")
+            "ü", "ue").replace("ß", "ss").replace("•", "").replace("™", "")
         os.rename(f"{path_dir}//{flag}", f"{path_dir}//{runner}")
 
 # Delete Author
 runner = False
 for runner in os.listdir(path_dir):
     if not ".py" in runner:
-        start = runner.find("(")
         if ".pdf" in runner:
-            new_name = f"{runner[0:start]}.pdf"
+            new_name = f"{runner[0:runner.find('(')]}.pdf"
         elif ".epub" in runner:
-            new_name = f"{runner[0:start]}.epub"
+            new_name = f"{runner[0:runner.find('(')]}.epub"
         else:
             print(f"Error\n\t-->{runner}")
             sys.exit(0)
@@ -49,8 +71,6 @@ for runner in os.listdir(path_dir):
             runner = runner.replace(" .pdf", ".pdf")
         elif ".epub" in runner:
             runner = runner.replace(" .epub", ".epub")
-        else:
-            print(f"Error\n\t-->\t{runner}")
         runner = runner.replace(" ", "_")
         os.rename(f"{path_dir}//{flag}", f"{path_dir}//{runner}")
 
@@ -59,13 +79,10 @@ runner = False
 for runner in os.listdir(path_dir):
     if not ".py" in runner:
         flag = runner
-        if ".pdf" in runner:
-            runner = runner.replace("pdf", "")
-            runner += ".pdf"
-        elif ".epub" in runner:
-            runner = runner.replace("epub", "")
-            runner += ".epub"
+        if "pdf" in runner:
+            end = ".pdf"
         else:
-            print(f"Error\n\t-->\t{runner}")
-        runner = runner.replace(" ", "_")
+            end = ".epub"
+        runner = f"{runner[0:runner.find('.')]}{end}"
+        print(runner)
         os.rename(f"{path_dir}//{flag}", f"{path_dir}//{runner}")
