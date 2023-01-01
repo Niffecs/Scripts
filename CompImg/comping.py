f51bead3dfcf9374f28296e8395319429cee775a
Niffecs <117777659+Niffecs@users.noreply.github.com>
2023-01-01 15:40:34 +0100

    Update comping.py

--- CompImg/comping.py
+++ CompImg/comping.py
@@ -1,52 +1,17 @@
-import filecmp
 import os
-import shutil
-from os import listdir
-from os.path import isfile, join
+import hashlib
 
-import shortuuid
-
-import helper
-
-# Create Session ID
-id = shortuuid.ShortUUID().random(length=6)
-
-# Legt einen Temp Folder an
-os.mkdir('tmp_'+id)
-
-# Liest den Ordner ein :)
-onlyfiles = [f for f in listdir(
-    str(os.getcwd())) if isfile(join(str(os.getcwd()), f))]
-print("Files:", onlyfiles, "\n")
-
-
-# Erlaubte Datei Typen
-datatype = ["png", "jpg", "PNG", "JPG"]
-
-# Test Output
-print("Types", datatype, "\n")
-
-
-# Filtern der Daten
-data = []
-for runner in onlyfiles:
-    if runner[-3::] in datatype:
-        data.append(runner)
-
-# Test Output
-print("Data", data, "\n")
-
-# Läuft alle Datein durch
-for base in data:
-    for test in data:
-        if not base.__eq__(test):
-            if filecmp.cmp(base, test):
-                # Test Layer
-                print(f"{base} <--> {test}")
-                shutil.move(helper.os_bindings(
-                    f"{os.getcwd()}/{test}"), helper.os_bindings(f"{os.getcwd()}/tmp_{id}/{test}"))
-                new_data = []
-                for runer in data:
-                    if runner != test:
-                        new_data.append(runer)
-                data = new_data
+def remove_duplicate_images(folder_path):
+    # Zunächst werden alle Bilddateien im Ordner eingelesen
+    image_files = [f for f in os.listdir(folder_path) if f.endswith(".jpg") or f.endswith(".png")]
+    
+    # Für jedes Bild wird ein Hash-Wert berechnet
+    image_hashes = {}
+    for file in image_files:
+        with open(os.path.join(folder_path, file), "rb") as f:
+            hash = hashlib.sha256(f.read()).hexdigest()
+            # Wenn es bereits ein Bild mit demselben Hash gibt, wird es gelöscht
+            if hash in image_hashes:
+                os.remove(os.path.join(folder_path, file))
+            else:
+                image_hashes[hash] = file
